b8360f699215c2e3ad0c7fb1f9c81d908a3b9fbe
Connor Hewitt <connor.hewitt@gmail.com>
2022-07-26 17:01:30 -0500

    Add test for fica deduction.  Fix bug for gross vs net salary

--- src/income.rs
+++ src/income.rs
@@ -24,38 +24,40 @@ pub struct RaiseSettings {
 
 pub struct JobSettings {
     // name, 401k, pension
-    starting_monthly_salary: f64,
+    starting_gross_income: f64,
     fica: Fica,
     raise: RaiseSettings,
 }
 
 pub struct Job {
-    starting_monthly_salary: f64,
-    income: Vec<f64>,
+    starting_gross_income: f64,
+    gross_income: Vec<f64>,
+    net_income: Vec<f64>,
     fica: Fica,
     raise: RaiseSettings,
     rates: Vec<Rate>
 }
 
 impl JobSettings {
-    pub fn new(starting_monthly_salary: f64, fica: Fica, raise: RaiseSettings) -> JobSettings {
-        JobSettings { starting_monthly_salary, fica, raise }
+    pub fn new(starting_gross_income: f64, fica: Fica, raise: RaiseSettings) -> JobSettings {
+        JobSettings { starting_gross_income, fica, raise }
     }
 
     pub fn create_job(&self, lifespan: Lifespan, rates: Vec<Rate>) -> Job {
-        let income = vec![0.0; lifespan.periods()];
+        let gross_income = vec![0.0; lifespan.periods()];
+        let net_income = vec![0.0; lifespan.periods()];
         assert_eq!(lifespan.periods(), rates.len());
 
-        Job { starting_monthly_salary: self.starting_monthly_salary, income, fica: self.fica, raise: self.raise, rates }
+        Job { starting_gross_income: self.starting_gross_income, gross_income, net_income, fica: self.fica, raise: self.raise, rates }
     }
 }
 
 impl IncomeProvider for Job {
     fn calculate_income_for_period(&mut self, period: Period /*, tax */) {
-        let salary = if period.get() == 0 {
-            self.starting_monthly_salary
+        let gross = if period.get() == 0 {
+            self.starting_gross_income
         } else if !period.is_new_year() {
-            self.income[period.get() - 1]
+            self.gross_income[period.get() - 1]
         } else {
             simplifying_assumption!("You get a raise every 12 months after the beginning of the \
                 simulation.  As long as the raise parameters are accurate, this should approximate \
@@ -66,22 +68,24 @@ impl IncomeProvider for Job {
                 inflation_adjustment = self.rates[period.get()-12..period.get()].iter().map(|r| r.inflation()).product::<f64>();
             }
 
-            self.income[period.get() - 1] * self.raise.amount * inflation_adjustment
+            self.gross_income[period.get() - 1] * self.raise.amount * inflation_adjustment
         };
 
-        let salary = match self.fica {
-            Fica::Participant { ss_rate } => { salary * (1.0 - ss_rate) },
-            Fica::Exempt => { salary }
+        self.gross_income[period.get()] = gross;
+
+        let net = match self.fica {
+            Fica::Participant { ss_rate } => { gross * (1.0 - ss_rate) },
+            Fica::Exempt => { gross }
         };
 
         // TODO contribute to social security/pension/401k
         // TODO tax
 
-        self.income[period.get()] = salary;
+        self.net_income[period.get()] = net;
     }
 
     fn get_net_income(&self) -> &Vec<f64> {
-        &self.income
+        &self.net_income
     }
 }
 
@@ -130,5 +134,19 @@ mod tests {
         }
 
         assert_vecfloat_absolute(job.get_net_income().clone(), vec![1024.0, 1024.0, 1024.0, 1024.0, 1024.0, 1024.0, 1024.0, 1024.0, 1024.0, 1024.0, 1024.0, 1024.0, 1114.401155525, 1114.401155525, 1114.401155525, 1114.401155525]);
+    }
+
+    #[test]
+    pub fn calculateincome_fica_raiseinflation_notax() {
+        let job_settings = JobSettings::new(1024.0, Fica::Participant { ss_rate: 0.0625 }, RaiseSettings { amount: 1.0625, adjust_for_inflation: true } );
+        let lifespan = Lifespan::new(16);
+        let rates = vec![Rate::new(1.0, 1.0, 1.002); 16];
+        let mut job = job_settings.create_job(lifespan, rates);
+
+        for period in lifespan.iter() {
+            job.calculate_income_for_period(period);
+        }
+
+        assert_vecfloat_absolute(job.get_net_income().clone(), vec![960.0, 960.0, 960.0, 960.0, 960.0, 960.0, 960.0, 960.0, 960.0, 960.0, 960.0, 960.0, 1044.751083304, 1044.751083304, 1044.751083304, 1044.751083304]);
     }
 }
