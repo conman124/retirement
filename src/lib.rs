ab72e2dcdb4fa370d40ee411fd4e4ac7a2ac492b
Connor Hewitt <connor.hewitt@gmail.com>
2022-08-30 21:13:07 -0500

    Remove wee_alloc (apparently it leaks memory lol)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
 #![allow(dead_code)]
 
-extern crate wee_alloc;
-
-// Use `wee_alloc` as the global allocator.
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 mod rates;
 mod assets;
 mod montecarlo;
