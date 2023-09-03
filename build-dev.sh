e6df610686d3f3bd80f1f05bb1321327d264af4a
Connor Hewitt <connor.hewitt@gmail.com>
2023-09-02 21:38:35 -0500

    Add main to package.json

--- build-dev.sh
+++ build-dev.sh
@@ -4,6 +4,6 @@ wasm-pack build --dev
 
 # add {"type": "module"} to the generated package.json
 # this makes it so that node can import it
-jq ". + {type: \"module\"}" pkg/package.json > pkg/package.json.tmp
+jq ". + {type: \"module\", main: \"retirement.js\"}" pkg/package.json > pkg/package.json.tmp
 mv pkg/package.json.tmp pkg/package.json
 cp .npmrc pkg
